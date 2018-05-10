fca1aa991896839780a86849bb23087d9895c87f
Garrett Johnson <38697020+BoardNinja@users.noreply.github.com>
2018-05-10 14:25:41 -0600

    Add files via upload

---
+++ RaceDay.js
@@ -0,0 +1,16 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = true;
+const age = 18;
+	if (age > 18 && raceNumber === true){
+console.log(raceNumber += 1000);
+  }
+  if (age > 18 && registeredEarly) {
+    console.log('You will race at 9:30 a.m.');
+  } else if (age && !registeredEarly){
+    console.log('You will race at 11:00 a.m.');
+  } else if (age < 18) {
+    console.log('You will race at 12:30 p.m.');
+  } else (age === 18) 
+    console.log('Go to the registration desk');
+  
+     
