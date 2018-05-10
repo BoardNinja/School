407b522aafc512a445dc7b4b02ea30e9509ec61f
Garrett Johnson <38697020+BoardNinja@users.noreply.github.com>
2018-05-10 14:29:09 -0600

    Add files via upload

---
+++ DogYears.js
@@ -0,0 +1,10 @@
+const myAge = 27;
+let earlyYears = 2;//This //
+earlyYears *= 10.5;
+laterYears = myAge - 2;//subtraction
+laterYears *= 4;//Calculate the number of dog years in one step
+console.log(earlyYears)
+console.log(laterYears)
+const myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Garrett'.toLowerCase();
+console.log(`My name is ${myName}.I am ${myAgeInDogYears}years old in dog years. `);
