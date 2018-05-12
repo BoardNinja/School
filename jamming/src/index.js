1c78ef148dc6294fe9549177c3389090cb9b3916
Garrett Johnson <38697020+BoardNinja@users.noreply.github.com>
2018-05-12 18:44:01 -0500

    Update index.js

--- jamming/src/index.js
+++ jamming/src/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from  'App';
 
-class Index extends React.Component {
-  render(){
-    return(
-      <div id="root"></div>
-    )
-  }
-}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
